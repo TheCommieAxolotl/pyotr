33799cda45ca6aba262867a68e55280ce3fa23f1
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2023-08-17 07:18:47 +1000

    Update runner test

--- test/standalone.run.js
+++ test/standalone.run.js
@@ -7,18 +7,12 @@ server.attach(route("/", () => html`<h1>Hello World</h1>`));
 server.attach(route("/json", () => json({ hello: "world" })));
 
 server.attach(
-    route("/path/[e]", (e) => {
+    route("/[...path]", (e) => {
         return json({
             params: e.params,
             query: e.query,
             method: e.method,
             path: e.path,
         });
-    })
-);
-
-server.attach(
-    route("/[...404]", () => {
-        return status(404);
     })
 );
